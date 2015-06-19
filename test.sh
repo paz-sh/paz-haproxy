a3b1e8a53aa1f835dbf65da97c38526efc060f60
Andrew Martin <sublimino@gmail.com>
2015-06-19 17:39:04 +0100

    Correct test.sh regex for etcd v2

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ alias fig='docker-compose'
 PAZ_IP=${DOCKER_HOST:-$(ip route get 1 | awk '{print $NF;exit}')}
 
 echo PAZ_IP $PAZ_IP
-sed -E "s/(ETCD_ADDR|ETCD_PEER_ADDR)=([^:]+)/\1=${PAZ_IP}/g" -i.bak fig.yml 
+sed -E "s/(ETCD_ADVERTISE_CLIENT_URLS)=.*/\1=http:\/\/${PAZ_IP}:2379/g" -i.bak fig.yml 
 
 fig stop
 fig build
